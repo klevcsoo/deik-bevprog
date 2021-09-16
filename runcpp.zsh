966543f77e8e772a666feb964d6de1b7df8eef03
Keve Lajsz <kevelajsz@icloud.com>
2021-09-16 14:51:57 +0200

    Added runnable file removal to runcpp.zsh

--- runcpp.zsh
+++ runcpp.zsh
@@ -2,9 +2,13 @@
 set -e
 
 ext=${1##*.}
+outfile=${1%%.*}
+
 if [ ${ext} != "cpp" ]; then
   echo "Extension must be .cpp"
   exit 1
 fi
 
-gcc $1 -o "${1%%.*}" -lstdc++
+gcc ${1} -o ${outfile} -lstdc++
+${outfile}
+rm ${outfile}
