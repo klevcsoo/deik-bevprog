846a486e98cd8c6d0ed60f94dab7e4cabdf445d5
Keve Lajsz <kevelajsz@icloud.com>
2021-09-22 15:53:13 +0200

    Added 1st drill

---
+++ Drillek/CH02/hello_world.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "string"
+#include "vector"
+#include "algorithm"
+#include "cmath"
+
+using namespace std;
+
+inline void keep_window_open() {
+  char ch;
+  cin >> ch;
+}
+
+int main() {
+  cout << "Hello, World!" << endl;
+  keep_window_open();
+
+  return 0;
+}
